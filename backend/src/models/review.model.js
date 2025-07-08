61e1972259502b3713eefb414942bb5f3cb5bcb9
DHARANEESH R <143324223+DharshOz@users.noreply.github.com>
2025-07-08 19:28:10 +0530

    Update review.model.js

--- backend/src/models/review.model.js
+++ backend/src/models/review.model.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose';
 
 const replySchema = new mongoose.Schema({
   text: { type: String, required: true },
-  repliedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  repliedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now }
 });
 
 const reviewSchema = new mongoose.Schema({
-  CustomerId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+  CustomerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   review: { type: String },
   rating: { type: Number },
