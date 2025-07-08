1c4b04d5a05e27de3ae93a1a75405d408c8311e7
DHARANEESH R <143324223+DharshOz@users.noreply.github.com>
2025-07-08 15:21:01 +0530

    Update review.model.js

--- backend/src/models/review.model.js
+++ backend/src/models/review.model.js
@@ -8,10 +8,10 @@ const replySchema = new mongoose.Schema({
 
 const reviewSchema = new mongoose.Schema({
   CustomerId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Food', required: true }, // ✅ Fixed ref
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   review: { type: String },
   rating: { type: Number },
-  images: [{ type: String }],
+  images: [{ type: String }], // <-- Add this line
   replies: [replySchema]
 }, { timestamps: true });
 
